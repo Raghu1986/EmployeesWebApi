434cbddd798534a27897f1f7fff0cd491f437b1e
Raghu1986 <Raghu1986@outlook.com>
2016-11-08 12:19:43 +0530

    Controller created

---
+++ CourseWebApi/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CourseWebApi.Models
+{
+    public class Course
+    {
+        [Key]
+        public int AutoId { get; set; }
+        public string CourseType { get; set; }
+        public string SubCourse { get; set; }
+        
+    }
+}
